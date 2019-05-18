d6042124f07f6194ef4aa6c3fe9a6db5d988d956
krustur <krustur@gmail.com>
2019-05-18 21:23:46 +0200

    Create raw_export.rs

---
+++ src/raw_export.rs
@@ -0,0 +1,13 @@
+use crate::ErrorKind;
+use std::fmt;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::mem;
+use std::ops::Range;
+use std::path;
+use std::str;
+
+pub fn write_file() {
+    println!("exporting!!!");
+}
